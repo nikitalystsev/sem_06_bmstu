af86d8cbab9b0534612ff6f4b1f3dcbf234d9ef5
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-03-02 00:46:36 +0300

    сделал вторую задачу

--- model/labs/lab_01/src/main.py
+++ model/labs/lab_01/src/main.py
@@ -1,4 +1,5 @@
 import task_01
+import task_02
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,44 +10,51 @@ def main() -> None:
     Главная функция
     """
 
-    f_row = task_01.get_u_x_by_row(0)
-    f_euler = task_01.get_u_x_by_euler()
-    f_picard1 = task_01.get_u_x_by_picard1()
-    f_picard2 = task_01.get_u_x_by_picard2()
+    # f_row = task_01.get_u_x_by_row(0)
+    # f_euler = task_01.get_u_x_by_euler()
+    # f_picard1 = task_01.get_u_x_by_picard1()
+    # f_picard2 = task_01.get_u_x_by_picard2()
+    #
+    # x_values: np.ndarray | tuple[np.ndarray, float | None] = np.linspace(-2, 6, 100)
+    # y_values_picard2: np.ndarray | tuple[np.ndarray, float | None] = f_picard2(x_values)
+    # y_values_row: np.ndarray | tuple[np.ndarray, float | None] = f_row(x_values)
+    #
+    # x_min = min(x_values)
+    # x_max = max(x_values)
+    # y_min = min(min(y_values_picard2), min(y_values_row))
+    # y_max = max(max(y_values_picard2), max(y_values_row))
+    #
+    # fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+    #
+    # # Постройте первый график
+    # axs[0].plot(x_values, y_values_picard2, color='blue')
+    # axs[0].set_title('Метод Пикара (2-е приближение)')
+    # axs[0].set_xlim(x_min, x_max)
+    # axs[0].set_ylim(y_min, y_max)
+    # axs[0].grid(True)
+    #
+    # # Постройте второй график
+    # axs[1].plot(x_values, y_values_row, color='red')
+    # axs[1].set_title('Разложение в ряд Тейлора (первые 5 членов)')
+    # axs[1].set_xlim(x_min, x_max)
+    # axs[1].set_ylim(y_min, y_max)
+    # axs[1].grid(True)
+    #
+    # # Отобразите все графики
+    # plt.tight_layout()  # Распределить графики равномерно по окну
+    # plt.show()
+    #
+    # print(f_row(0.1))
+    # print(f_euler(0.1))
+    # print(f_picard1(0.1))
+    # print(f_picard2(0.1))
 
-    x_values: np.ndarray | tuple[np.ndarray, float | None] = np.linspace(-2, 6, 100)
-    y_values_picard2: np.ndarray | tuple[np.ndarray, float | None] = f_picard2(x_values)
-    y_values_row: np.ndarray | tuple[np.ndarray, float | None] = f_row(x_values)
-
-    x_min = min(x_values)
-    x_max = max(x_values)
-    y_min = min(min(y_values_picard2), min(y_values_row))
-    y_max = max(max(y_values_picard2), max(y_values_row))
-
-    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-
-    # Постройте первый график
-    axs[0].plot(x_values, y_values_picard2, color='blue')
-    axs[0].set_title('Метод Пикара (2-е приближение)')
-    axs[0].set_xlim(x_min, x_max)
-    axs[0].set_ylim(y_min, y_max)
-    axs[0].grid(True)
-
-    # Постройте второй график
-    axs[1].plot(x_values, y_values_row, color='red')
-    axs[1].set_title('Разложение в ряд Тейлора (первые 5 членов)')
-    axs[1].set_xlim(x_min, x_max)
-    axs[1].set_ylim(y_min, y_max)
-    axs[1].grid(True)
-
-    # Отобразите все графики
-    plt.tight_layout()  # Распределить графики равномерно по окну
-    plt.show()
-
-    print(f_row(0.1))
-    print(f_euler(0.1))
-    print(f_picard1(0.1))
-    print(f_picard2(0.1))
+    print(task_02.get_x_u_by_analyt(0.3))
+    print(task_02.get_x_u_by_picard1(0.3))
+    print(task_02.get_x_u_by_picard2(0.3))
+    print(task_02.get_x_u_by_picard3(0.3))
+    print(task_02.get_x_u_by_picard4(0.3))
+    # print(task_02.get_x_u_by_picard5(0.3))
 
 
 if __name__ == '__main__':
--- model/labs/lab_01/src/task_02.py
+++ model/labs/lab_01/src/task_02.py
@@ -1,179 +1,52 @@
-import matplotlib.pyplot as plt
-
-BASE = "\x1B[0m"
-GREEN = "\x1B[32m"
-RED = "\x1B[31m"
-YELLOW = "\x1B[33m"
-BLUE = "\x1B[34m"
-PURPLE = "\x1B[35m"
-
-MAX_X = 2.003
-STEP = 1e-4
+import math as m
 
 
-def f(x, y):
-    return pow(x, 2) + pow(y, 2)
+# 1 - 2 * x * u * u' = u ^ 3 * u'
+# u(0.5) = 0
+
+def get_x_u_by_analyt(u: int | float) -> int | float:
+    """
+    Функция аналитического решения
+    """
+
+    return m.exp(u ** 2) - (u ** 2) / 2 - 1 / 2
 
 
-def PicarApprox1(x):
-    return pow(x, 3) / 3
+def get_x_u_by_picard1(u: int | float) -> int | float:
+    """
+    Функция решения методом Пикара (1-е приближение)
+    """
+
+    return 0.5 + (u ** 2) / 2 + (u ** 4) / 4
 
 
-def PicarApprox2(x):
-    return PicarApprox1(x) + \
-        pow(x, 7) / 63
+def get_x_u_by_picard2(u: int | float) -> int | float:
+    """
+    Функция решения методом Пикара (2-е приближение)
+    """
+
+    return 0.5 + (u ** 16) / 12 + (u ** 4) / 2 + (u ** 2) / 2
 
 
-def PicarApprox3(x):
-    return PicarApprox2(x) + \
-        2 * pow(x, 11) / 2079 + \
-        pow(x, 15) / 59535
+def get_x_u_by_picard3(u: int | float) -> int | float:
+    """
+    Функция решения методом Пикара (3-е приближение)
+    """
+
+    return 0.5 + (u ** 18) / 108 + (u ** 6) / 6 + (u ** 4) / 2 + (u ** 2) / 2
 
 
-def PicarApprox4(x):
-    return PicarApprox2(x) + \
-        2 * pow(x, 11) / 2079 + \
-        13 * pow(x, 15) / 218295 + \
-        82 * pow(x, 19) / 37328445 + \
-        662 * pow(x, 23) / 10438212015 + \
-        4 * pow(x, 27) / 3341878155 + \
-        pow(x, 31) / 109876903975
+def get_x_u_by_picard4(u: int | float) -> int | float:
+    """
+    Функция решения методом Пикара (4-е приближение)
+    """
+
+    return 0.5 + (u ** 20) / 1080 + (u ** 8) / 24 + (u ** 6) / 6 + (u ** 4) / 2 + (u ** 2) / 2
 
 
-def Picar(x_max, h, PicarApprox):
-    result = []
-    x, y = 0, 0
+def get_x_u_by_picard5(u: int | float) -> int | float:
+    """
+    Функция решения методом Пикара (4-е приближение)
+    """
 
-    while abs(x) < abs(x_max):
-        result.append(y)
-        x += h
-        y = PicarApprox(x)
-
-    return result
-
-
-def Euler(x_max, h):
-    result = []
-    x, y = 0, 0
-
-    while abs(x) < abs(x_max):
-        result.append(y)
-        y = y + h * f(x, y)
-        x += h
-
-    return result
-
-
-def RungeKutta(x_max, h):
-    result = []
-    coeff = h / 2
-    x, y = 0, 0
-
-    while abs(x) < abs(x_max):
-        result.append(y)
-        y = y + h * f(x + coeff, y + coeff * f(x, y))
-        x += h
-
-    return result
-
-
-def generate_x(x_max, step):
-    result = []
-    x = 0
-
-    while abs(x) < abs(x_max):
-        result.append(round(x, 3))
-        x += step
-
-    return result
-
-
-def print_res_table(x_arr, picar_approx1_arr, picar_approx2_arr,
-                    picar_approx3_arr, picar_approx4_arr,
-                    euler_arr, runge_kutta):
-    print("\n%s  X   | PicarApprox1 | PicarApprox2 | PicarApprox3 | PicarApprox4 |     Euler    | RungeKutta \n"
-          "-----------------------------------------------------------------------------------------------%s"
-          % (PURPLE, BASE))
-
-    for i in range(len(x_arr)):
-        if i % 500 == 0:
-            print("%5.2f %s|%s%12.5f  %s|%s%12.5f  %s|%s%12.5f  %s|%s%12.5f  %s|%s%12.5f  %s|%s%12.5f  " \
-                  % (x_arr[i], PURPLE, BASE,
-                     picar_approx1_arr[i], PURPLE, BASE,
-                     picar_approx2_arr[i], PURPLE, BASE,
-                     picar_approx3_arr[i], PURPLE, BASE,
-                     picar_approx4_arr[i], PURPLE, BASE,
-                     euler_arr[i], PURPLE, BASE,
-                     runge_kutta[i]
-                     ))
-
-    print()
-
-
-def build_graph(x_arr, picar_approx1_arr, picar_approx2_arr,
-                picar_approx3_arr, picar_approx4_arr,
-                euler_arr):
-    fig1 = plt.figure(figsize=(10, 7))
-    plot = fig1.add_subplot()
-    plot.plot(x_arr, picar_approx1_arr, label="PicarApprox1")
-    plot.plot(x_arr, picar_approx2_arr, label="PicarApprox2")
-    plot.plot(x_arr, picar_approx3_arr, label="PicarApprox3")
-    plot.plot(x_arr, picar_approx4_arr, label="PicarApprox4")
-    plot.plot(x_arr, euler_arr, label="Euler")
-
-    plt.legend()
-    plt.grid()
-    plt.title("Сравнение алгоритмом")
-
-    plt.show()
-
-
-def main():
-    x_arr = generate_x(MAX_X, STEP)
-    print(x_arr)
-    picar_approx1_arr = Picar(MAX_X, STEP, PicarApprox1)
-    picar_approx2_arr = Picar(MAX_X, STEP, PicarApprox2)
-    picar_approx3_arr = Picar(MAX_X, STEP, PicarApprox3)
-    picar_approx4_arr = Picar(MAX_X, STEP, PicarApprox4)
-    euler_arr = Euler(MAX_X, STEP)
-    runge_kutta = RungeKutta(MAX_X, STEP)
-
-    print_res_table(x_arr, picar_approx1_arr, picar_approx2_arr,
-                    picar_approx3_arr, picar_approx4_arr,
-                    euler_arr, runge_kutta)
-
-    x_arr = generate_x(-MAX_X, -STEP)
-    x_arr.reverse()
-    x_arr.extend(generate_x(MAX_X, STEP))
-
-    picar_approx1_arr = Picar(-MAX_X, -STEP, PicarApprox1)
-    picar_approx1_arr.reverse()
-    picar_approx1_arr.extend(Picar(MAX_X, STEP, PicarApprox1))
-
-    picar_approx2_arr = Picar(-MAX_X, -STEP, PicarApprox2)
-    picar_approx2_arr.reverse()
-    picar_approx2_arr.extend(Picar(MAX_X, STEP, PicarApprox2))
-
-    picar_approx3_arr = Picar(-MAX_X, -STEP, PicarApprox3)
-    picar_approx3_arr.reverse()
-    picar_approx3_arr.extend(Picar(MAX_X, STEP, PicarApprox3))
-
-    picar_approx4_arr = Picar(-MAX_X, -STEP, PicarApprox4)
-    picar_approx4_arr.reverse()
-    picar_approx4_arr.extend(Picar(MAX_X, STEP, PicarApprox4))
-
-    euler_arr = Euler(-MAX_X, -STEP)
-    euler_arr.reverse()
-    euler_arr.extend(Euler(MAX_X, STEP))
-
-    runge_kutta = RungeKutta(-MAX_X, -STEP)
-    runge_kutta.reverse()
-    runge_kutta.extend(RungeKutta(MAX_X, STEP))
-
-    build_graph(x_arr, picar_approx1_arr, picar_approx2_arr,
-                picar_approx3_arr, picar_approx4_arr,
-                euler_arr)
-
-
-if __name__ == "__main__":
-    main()
+    return 0.5 + (u ** 22) / 1080 + (u ** 10) / 24 + (u ** 8) / 6 + (u ** 6) / 2 + 3 * (u ** 4) / 4 + (u ** 2) / 2
