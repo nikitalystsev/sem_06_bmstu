52a2a6208849b62d28a6f5c8bcb64a887d159bc8
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-03-02 11:02:21 +0300

    правки

--- model/labs/lab_01/src/task_01.py
+++ model/labs/lab_01/src/task_01.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # u'' + 0.1 * (u')^2 + (1 + 0.1 * x) * u = 0
 # u(0) = 1, u'(0) = 2
 
-# раскладываем в ряд Тейлора и находим коэффициенты при первых 5-ти членах
+# раскладываем в ряд Тейлора и находим коэффициенты при первых пяти членах
 
 def get_first() -> int:
     """
@@ -49,7 +49,7 @@ def get_fifth(x_0: float) -> float:
     """
 
     _sum = 0.2 * (get_third(x_0) ** 2 + get_second() * get_fourth(x_0)) + get_third(x_0) + 0.1 * get_second() + \
-           0.1 * (get_second() + x_0 * get_third(x_0))
+        0.1 * (get_second() + x_0 * get_third(x_0))
 
     return -_sum
 
@@ -60,9 +60,9 @@ def get_u_x_by_row(x_0: float):
     """
 
     str_u_x = f"Разложением в ряд: u(x) = {get_first()} + {get_second() / 1: .3f} * x + " \
-              f"{get_third(x_0) / 2: .3f} * x^2 +" \
-              f" {get_fourth(x_0) / 6: .3f} * x^3 + " \
-              f"{get_fifth(x_0) / 24: .3f} * x^4"
+        f"{get_third(x_0) / 2: .3f} * x^2 +" \
+        f" {get_fourth(x_0) / 6: .3f} * x^3 + " \
+        f"{get_fifth(x_0) / 24: .3f} * x^4"
 
     print(str_u_x)
 
