a4689ac5781cd4e714631017c49d9553c2400450
nikitalystsev <nikitalystsev@yandex.ru>
2024-03-08 11:36:00 +0300

    сделал первую вариацию 1- части третьей лабы

--- os/labs/lab_03/src/client.c
+++ os/labs/lab_03/src/client.c
@@ -16,6 +16,28 @@
 
 int main(void)
 {
-    /* code */
+    int sock;
+    struct sockaddr addr;
+
+    if ((sock = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("Ошибка socket");
+        exit(1);
+    }
+
+    addr.sa_family = AF_UNIX;
+    strcpy(addr.sa_data, "./test_sock");
+
+    char buf[128];
+    snprintf(buf, 128, "Client (pid %d) send message", getpid());
+
+    if (sendto(sock, buf, strlen(buf) + 1, 0, &addr, sizeof(addr)) == -1)
+    {
+        perror("Ошибка sendto");
+        exit(1);
+    }
+
+    close(sock);
+
     return 0;
 }
--- os/labs/lab_03/src/server.c
+++ os/labs/lab_03/src/server.c
@@ -16,6 +16,41 @@
 
 int main(void)
 {
-    /* code */
+    int sock;
+    struct sockaddr addr;
+    char buf[1024];
+    int bytes_read;
+
+    if ((sock = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1)
+    {
+        perror("Ошибка socket");
+        exit(1);
+    }
+
+    addr.sa_family = AF_UNIX;
+    strcpy(addr.sa_data, "./test_sock");
+
+    if (bind(sock, &addr, sizeof(addr)))
+    {
+        perror("Ошибка bind");
+        exit(1);
+    }
+
+    while (1)
+    {
+        if ((bytes_read = recvfrom(sock, buf, 1024, 0, NULL, NULL)) == -1)
+        {
+            perror("Ошибка recvfrom");
+            exit(1);
+        }
+
+        buf[bytes_read] = '\0';
+        printf("Server get message: %s\n", buf);
+
+        sleep(1);
+    }
+
+    close(sock);
+
     return 0;
 }
