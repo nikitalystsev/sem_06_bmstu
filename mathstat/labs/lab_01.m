d9f7b927397e2445e02d27a009795501bea44e6e
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2024-03-22 11:14:22 +0300

    делаю...

--- mathstat/labs/lab_01.m
+++ mathstat/labs/lab_01.m
@@ -1,3 +1,13 @@
-x = -100:0.01:10;
-y = sin(x);
-plot(x, y);
+function lab_01()
+    X = load('sel.txt');
+
+    X = sort(X);
+    
+    fprintf("a) Вычисление максимального значения Mmax и минимального значения Mmin\n");
+    
+    Mmax = max(X);
+    Mmin = min(X);
+
+    fprintf("\nMmax = %.4f\n", Mmax);
+    fprintf("\nMmin  = %.4f\n", Mmin);
+end
