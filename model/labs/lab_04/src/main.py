9910f4ed1833eb57411315e503dd92d6d1a65ccf
nikitalystsev <nikitalystsev@yandex.ru>
2024-05-12 14:56:20 +0300

    add meeting sweep

--- model/labs/lab_04/src/main.py
+++ model/labs/lab_04/src/main.py
@@ -244,24 +244,87 @@ def left_sweep(t_m, curr_time, data: Data):
     return u
 
 
+def meetings_sweep(t_m, curr_time, data: Data, n_eq):
+    """
+    Реализация встречной прогонки
+    """
+    b, h, tau = data.b, data.h, data.tau
+
+    # Прямой ход
+    k_0, m_0, p_0 = left_boundary_condition(t_m, curr_time, data)
+    k_n, m_n, p_n = right_boundary_condition(t_m, curr_time, data)
+
+    # правая часть прогонки
+    x = h
+    n = 1
+
+    ksi_r = [0, -k_0 / m_0]
+    eta_r = [0, p_0 / m_0]
+
+    for i in range(1, n_eq):
+        a_n = kappa(t_m[i - 1], t_m[i]) * tau / h
+        d_n = kappa(t_m[i], t_m[i + 1]) * tau / h
+        b_n = a_n + d_n + _c(t_m[i]) * h + p(x) * h * tau
+        f_n = _c(t_m[i]) * t_m[i] * h + f(x, curr_time, t_m[i]) * h * tau
+
+        ksi_r.append(d_n / (b_n - a_n * ksi_r[n]))
+        eta_r.append((a_n * eta_r[n] + f_n) / (b_n - a_n * ksi_r[n]))
+
+        n += 1
+        x += h
+
+    # левая часть прогонки
+    ksi_l = [-k_n / m_n, 0]
+    eta_l = [p_n / m_n, 0]
+
+    x = b - h
+    n1 = -2
+    cnt = 1
+
+    for i in range(len(t_m) - 2, n_eq - 1, -1):
+        a_n = kappa(t_m[i - 1], t_m[i]) * tau / h
+        d_n = kappa(t_m[i], t_m[i + 1]) * tau / h
+        b_n = a_n + d_n + _c(t_m[i]) * h + p(x) * h * tau
+        f_n = _c(t_m[i]) * t_m[i] * h + f(x, curr_time, t_m[i]) * h * tau
+
+        ksi_l.insert(0, a_n / (b_n - d_n * ksi_l[n1]))
+        eta_l.insert(0, (d_n * eta_l[n1] + f_n) / (b_n - d_n * ksi_l[n1]))
+
+        n1 -= 1
+        x -= h
+        cnt += 1
+
+    # # Обратный ход
+    u = [0] * (n + cnt)
+
+    # вычисляем up (решая систему из двух уравнений) -- сопряжение решений
+    u[n_eq] = (ksi_r[-1] * eta_l[0] + eta_r[-1]) / (1 - ksi_r[-1] * ksi_l[0])
+
+    for i in range(n_eq - 1, -1, -1):
+        u[i] = ksi_r[i + 1] * u[i + 1] + eta_r[i + 1]
+
+    for i in range(n_eq + 1, n + cnt):
+        _i = i - n_eq
+        u[i] = ksi_l[_i - 1] * u[i - 1] + eta_l[_i - 1]
+
+    return u
+
+
 def simple_iteration_on_layer(t_m, curr_time, data):
     """
     Вычисляет значение искомой функции (функции T) на слое t_m_plus_1
     """
     # print("[+] call simple_iteration_on_layer")
+
     _t_m = t_m
-    # print(f"len(t_m) = {len(t_m)}")
-    # print(f"t_m = {t_m}")
 
     while True:
         # цикл подсчета значений функции T методом простых итераций для
         # слоя t_m_plus_1
 
         # t_m_plus_1 = right_sweep(_t_m, curr_time, data)
-        t_m_plus_1 = left_sweep(_t_m, curr_time, data)
-
-        # print(f"t_m_plus_1[:20] = {t_m_plus_1[:20]}")
-        # input()
+        # t_m_plus_1 = left_sweep(_t_m, curr_time, data)
+        t_m_plus_1 = meetings_sweep(_t_m, curr_time, data, 300)
 
         cnt = 0
 
